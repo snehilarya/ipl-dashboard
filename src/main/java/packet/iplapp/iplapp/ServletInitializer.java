ddb637a26f516e98840a4c7887f56824eb6715e8
snehilarya <snehilarya1@gmail.com>
2021-04-19 01:49:30 +0530

    intial commit creating ipl dashboard

---
+++ src/main/java/packet/iplapp/iplapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+// package packet.iplapp.iplapp;
+
+// import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// public class ServletInitializer extends SpringBootServletInitializer {
+
+// 	@Override
+// 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+// 		return application.sources(IplappApplication.class);
+// 	}
+
+// }
